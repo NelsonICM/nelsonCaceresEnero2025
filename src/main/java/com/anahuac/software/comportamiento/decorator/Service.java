43900fd6c60736f7bc9a69e051f57ab2b90fe92c
NelsonICM <107224948+NelsonICM@users.noreply.github.com>
2025-02-14 19:57:25 -0600

    Delete src directory

--- src/main/java/com/anahuac/software/comportamiento/decorator/Service.java
+++
@@ -1,7 +0,0 @@
-package com.anahuac.software.comportamiento.decorator;
-
-// Interfaz base para los servicios
-public interface Service {
-    double getCost();
-    String getDescription();
-}
--- src/main/java/com/anahuac/software/creacional/factorymethod/Car.java
+++
@@ -1,9 +0,0 @@
-package com.anahuac.software.creacional.factorymethod;
-
-// Interfaz común para todas las líneas de autos
-public interface Car {
-    void addBody();
-    void addDoors();
-    void paint();
-    void assemble(); // Método que sigue el flujo de ensamblaje
-}
